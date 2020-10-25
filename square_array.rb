03d3cb7a6fab26ccb4b1a5e2ce0772870e149a2a
DavidNNussbaum <brocknynj@aol.com>
2020-10-25 17:36:42 -0400

    commit tree 914e68d2dc077ffae9b9cae9d18d3d6b21b64257

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
+require 'pry'
 def square_array(array)
+  new_array = []
+  array.each do |number|
+  new_array << number**2  
+  end
+  new_array  
   # your code here
 end
+
+
+# def square_array(array)
+#   array.each do |number|
+#     new_array = puts collect array {|number| number**2}
+#     new_array
+#   end
+#   # your code here
+# end
+
+ 
+  
+ 
